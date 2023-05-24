e54ffe123b486836ea9a2e758c8a27ded2f3cdab
RyunosukeSakurada <longzhizhu513@gmail.com>
2023-05-24 02:37:10 -0700

    connected with DB

--- api/server.js
+++ api/server.js
@@ -1,5 +1,16 @@
 const express = require('express');
 const PORT = 5000;
+const mongoose = require("mongoose");
+
 const app = express();
 
+
+mongoose.connect('mongodb+srv://MERN_BLOG:OST7omMEIJA8OhJM@cluster0.k1jdd8u.mongodb.net/')
+.then(() => {
+  console.log("Connecting with DataBase");
+}).catch(() => {
+  console.log("error");
+})
+
 app.listen(PORT, () => console.log("Server Started"))
+
